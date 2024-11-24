341c8c0e483e2890fd187c93bce47bf47c504664
kelemengqi <1565916105@qq.com>
2024-11-24 12:49:47 +0800

    Delete 331-backend_副本 directory

--- 331-backend_副本/src/main/java/se331/lab/repository/ParticipantRepository.java
+++
@@ -1,8 +0,0 @@
-package se331.lab.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import se331.lab.rest.entity.Participant;
-@Repository
-public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-}
